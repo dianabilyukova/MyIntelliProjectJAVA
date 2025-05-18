e12db3f33d341908de9be9d5a649663b69db9c42
Diana <dianabilyukova@gmail.com>
2025-05-18 10:34:08 +0300

    exercise

--- MyIntelliProject/src/FoodDelivery.java
+++ MyIntelliProject/src/FoodDelivery.java
@@ -4,6 +4,19 @@ public class FoodDelivery {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double numberOfChickenMenus = scanner.nextDouble();
+        double numberOfFishMenus = scanner.nextDouble();
+        double numberOfVegetarianMenus = scanner.nextDouble();
+
+        double priceForChickenMenus = numberOfChickenMenus * 10.35;
+        double priceForFishMenus = numberOfFishMenus * 12.40;
+        double priceForVegetarianMenus = numberOfVegetarianMenus * 8.15;
+        double totalPriceForMenus = priceForChickenMenus + priceForFishMenus + priceForVegetarianMenus;
+        double priceForDesert = totalPriceForMenus * 20 / 100;
+        double deliveryCost = 2.50;
+        double totalPrice = totalPriceForMenus + priceForDesert + deliveryCost;
+
+        System.out.println(totalPrice);
 
     }
 }
